f5486464dc584aaf8127c1f070c1494da2bd7642
Micah Levine <micah.levine@outlook.com>
2019-08-11 00:32:54 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,19 +1,13 @@
 def unsafe?(speed)
-  if speed < 40
-     true
-  else false
-    if speed > 60
-       true
-    else false
- end
+  if speed > 60
+    true
+  elsif speed < 40
+    true
+  else
+    false
+  end
 end
-end
-
-
 
 def not_safe?(speed)
-  speed < 40 ? true : false
-OR if
-  speed > 60 ? true : false
-end
+  speed < 40 || speed > 60 ? true : false
 end
